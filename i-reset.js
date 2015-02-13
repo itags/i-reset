77185f6fad426c89575bac52afd93fd09e298579
Marco Asbreuk <marco@itsasbreuk.nl>
2015-02-13 01:00:47 +0100

    intermediate saving

---
+++ i-reset.js
@@ -0,0 +1,36 @@
+module.exports = function (window) {
+    "use strict";
+
+    require('itags.core')(window);
+
+    var pseudoName = 'reset', // <-- define your own pseudo-name here
+        itagName = 'i-button#'+pseudoName, // <-- define your own itag-name here
+        Event = window.ITSA.Event,
+        Itag, IButton;
+
+    if (!window.ITAGS[itagName]) {
+
+        IButton = require('i-button')(window);
+
+        Event.before(itagName+':manualfocus', function(e) {
+            // the i-select itself is unfocussable, but its button is
+            // we need to patch `manualfocus`,
+            // which is emitted on node.focus()
+            // a focus by userinteraction will always appear on the button itself
+            // so we don't bother that
+            var element = e.target;
+            e.preventDefault();
+            element.itagReady().then(
+                function() {
+                    var button = element.getElement('button');
+                    button && button.focus(true, true);
+                }
+            );
+        });
+
+        Itag = IButton.pseudoClass(pseudoName);
+        window.ITAGS[itagName] = Itag;
+    }
+
+    return window.ITAGS[itagName];
+};
